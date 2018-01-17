b1b0a0f8a9e6d6604c19eefbb368778ad03705d1
Kyle Scagnelli <kjs5107@rit.edu>
2018-01-17 14:58:51 -0500

    Forgot to stage these files.

--- src/main/webapp/src/app/Payment/payment.component.ts
+++ src/main/webapp/src/app/Payment/payment.component.ts
@@ -1,3 +1,17 @@
 /**
  * Created by kylescagnelli on 1/17/18.
  */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'payment',
+  templateUrl: './payment.template.html',
+  styleUrls: [
+    './payment.style.css'
+  ]
+})
+export class PaymentComponent {
+
+
+
+}
--- src/main/webapp/src/app/app-routing.module.ts
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -5,14 +5,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from "./PageNotFound/page-not-found.component";
 import { LandingPageComponent } from "./LandingPage/landing-page.component";
+import { DashboardComponent } from "./Dashboard/dashboard.component";
+import { AccountComponent } from "./Account/account.component";
+import { BreweryComponent } from "./Brewery/brewery.component";
+import { BrewsComponent } from "./Brews/brews.component";
+import { PaymentComponent } from "./Payment/payment.component";
 
 /**
- * Defines the url paths for all the features in Tiger Center, with their components attached.
+ * Defines the url paths for all the features in OnDraftApp, with their components attached.
  * > Note: *IMPORTANT: ALWAYS LEAVE THE WILDCARD PATH ** AS THE LAST PATH DEFINED IN THIS LIST*
  */
 const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: 'landing', component: LandingPageComponent },
+  // TODO: set up angular guards for all features besides landing page.
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'brewery', component: BreweryComponent },
+  { path: 'brews', component: BrewsComponent },
+  { path: 'payment', component: PaymentComponent },
   // IMPORTANT: You must leave the following wildcard path as the last path no matter what. You've been warned.
   { path: '**', component: PageNotFoundComponent }
 ];
